5393ea31c547383e500fe2fb77db9391806497b0
thalavithu <38806113+thalavithu@users.noreply.github.com>
2018-05-24 20:10:36 +0200

    php bearbeitet

--- neuerbenutzer.php
+++ neuerbenutzer.php
@@ -1,45 +1,26 @@
 <?php
-/*
+
 //Variablen definieren
 $vorname = $_POST["vorname"];
 $nachname = $_POST["nachname"];
 $mail = $_POST["mail"];
 $kennwort = $_POST["kennwort"];
 
-//Variablen überprüfen
-If (empty ($mail)){
-	echo("<h3>Mailadresse muss angegeben werden</h3>");
-}
-If (empty ($kennwort)){
-	echo("<h3>Kennwort muss angegeben werden</h3>");
-}
-
-If ((!empty ($mail)) AND (!empty ($kennwort)) ){
-
-    //auf DB connecten
-    $connect = mssql_connect("ibz-tagebuch-mysqldbserver.database.windows.net,1433", "mysqldbuser", "LoveIBZ$2018$"); 
-
-    //Datenbank auswählen
-    mssql_select_db('IBZ-Tagebuch-SQL'); 
-
-    //Daten in DB schreiben
-    $sql = ("INSERT INTO appuser (user_loginname, user_password, user_firstname, user_surname, user_money) VALUES ('$vorname', '$nachname', '$mail', '$kennwort');");
-    $daten = mssql_query($sql);
-}
-*/
-
+//Serververbindung
 $serverName = "ibz-tagebuch-mysqldbserver.database.windows.net,1433";
-$connectionOptions = array(
-    "Database" => "IBZ-Tagebuch-SQL",
-    "Uid" => "mysqldbuser",
-    "PWD" => "LoveIBZ$2018$"
-);
-
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+$connectionOptions = 
+    array(
+        "Database" => "IBZ-Tagebuch-SQL",
+        "Uid" => "mysqldbuser",
+        "PWD" => "LoveIBZ$2018$"
+    );
+$conn = sqlsrv_connect($serverName, $connectionOptions)or die('Error connecting to the SQL Server database.');
 
 
 if( $conn ) {
-    echo "Connection established.<br />";
+    $sql = ("INSERT INTO tagebuch.Benutzer (Vorname, Nachname, Mail, Kennwort) VALUES ('$vorname', '$nachname', '$mail', '$kennwort');");
+    $daten = mssql_query($sql);
+    header('Location: Index.html');
 }else{
     echo "Connection could not be established.<br />";
     die( print_r( sqlsrv_errors(), true));
