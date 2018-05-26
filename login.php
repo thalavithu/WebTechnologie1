93aa2a8112b16550fd6d47905911ba6d58f88d7c
thalavithu <38806113+thalavithu@users.noreply.github.com>
2018-05-26 11:40:28 +0200

    passwort muss eingegeben

--- login.php
+++ login.php
@@ -17,11 +17,7 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
 
 if( $conn ) {
     /* echo "Connection established.<br />"; */
-	
-	 
-     header('Location: tagebuch.html');
-    
-    
+  
 }else{
     echo "Connection could not be established.<br />";
     die( print_r( sqlsrv_errors(), true));
@@ -30,5 +26,5 @@ if( $conn ) {
 
 
 
-
+header('Location: tagebuch.html');
 ?>
