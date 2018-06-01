c0feada56d69dfe251bec1ea85696adf7c2d0bbb
thalavithu <38806113+thalavithu@users.noreply.github.com>
2018-06-01 21:32:12 +0200

    LoginWirdGecheckt

--- login.php
+++ login.php
@@ -2,41 +2,32 @@
 
 //Variablen definieren
 
-$mail = $_POST["mail"];
-$kennwort = $_POST["kennwort"];
+ $mail = $_POST["mail"];
+ $kennwort = $_POST["passwort"];
 
-//Serververbindung
-$serverName = "ibz-tagebuch-mysqldbserver.database.windows.net,1433";
+ $serverName = "ibz-tagebuch-mysqldbserver.database.windows.net,1433";
 $connectionOptions = 
     array(
         "Database" => "IBZ-Tagebuch-SQL",
         "Uid" => "mysqldbuser",
         "PWD" => "LoveIBZ$2018$"
     );
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-if( $conn ) {
-    /* echo "Connection established.<br />"; */
-    $sql = ("SELECT * FROM tagebuch.Benutzer WHERE Mail = '$mail' AND Kennwort = '$kennwort';");
-    $result = sqlsrv_query($conn, $sql);
-    $numrows = sqlsrv_num_rows($result);
-    echo $result;
-    /*
-    if($numrows > 0) {
-        echo "Zeile vorhanden";
-       // header('Location: tagebuch.html');
+    if ($conn) {
+        $sql = "SELECT vorname FROM tagebuch.Benutzer  WHERE Mail = '$mail' AND Kennwort = '$kennwort'";
+        echo $sql;
+        $stmt = sqlsrv_query($conn, $sql);
+        if ($stmt === false) {
+            echo "Connection to server failed...<br>";
+        } else {
+            
+            if (sqlsrv_has_rows($stmt) > 0) {
+                header('Location: tagebuch.html');
+     
+            } else {
+                echo "Failed to authenticate...<br>";
+            }
+        }
     }
-    else {
-        echo "Error in retrieveing row count.";
-    }
-   */
-  
-}else{
-    echo "Connection could not be established.<br />";
-    die( print_r( sqlsrv_errors(), true));
-}
-
-
-
-
 ?>
