b057ac25b6019a5f986df6a532cb0d0da9b70205
thalavithu <38806113+thalavithu@users.noreply.github.com>
2018-08-28 18:49:03 +0200

    Last Changes

--- neuereintrag2.php
+++ neuereintrag2.php
@@ -1,5 +1,7 @@
 <?php
-
+session_start();
+//Serververbindung
+include_once './config/sql.connection.php';
 
 //Variablen definieren
 $kategorie = $_POST["kategorie"];
@@ -9,7 +11,15 @@ $text = $_POST["text"];
 
 
 //Bildinfos holen
-$image = $_FILES['bild']['name'];
+//$image = $_FILES['bild']['name'];
+
+
+//BenutzerID auslesen
+$sql1 = ("SELECT BenutzerId FROM tagebuch.Benutzer WHERE Mail = '{$_SESSION['Mail']}';");
+$daten1 = sqlsrv_query($conn, $sql1);
+while($result1 = sqlsrv_fetch_object($daten1)){
+    $benutzerId = $result1->BenutzerId;
+}
 
 $sql3 = ("INSERT INTO tagebuch.Tagebuch (BenutzerID, KategorieID, Bild, Datum, Freitext) VALUES ('$benutzerId', '$kategorie', '$image', '$datum', '$text');");
 
